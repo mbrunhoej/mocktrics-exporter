4606bec7c7454657db8c062d7c6316b4a70063c6
mlb <mbrunhoej@proton.me>
2025-08-28 19:08:04 +0000

    Fixed run-prometheus script

--- install-prometheus.sh
+++
@@ -1,4 +0,0 @@
-
-curl -L https://github.com/prometheus/prometheus/releases/download/v3.6.0-rc.0/prometheus-3.6.0-rc.0.linux-amd64.tar.gz -o \
-  /tmp/prometheus-3.6.0-rc.0.linux-amd64.tar.gz
-tar -xvf prometheus-3.6.0-rc.0.linux-amd64.tar.gz
