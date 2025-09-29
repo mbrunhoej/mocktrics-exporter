f60c1044ec8a663a9b2c7fd4eab4ecfdde779795
mlb <mbrunhoej@proton.me>
2025-09-29 20:04:50 +0000

    Add meta metrics

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -7,11 +7,16 @@ import mocktrics_exporter
 @pytest.fixture(autouse=True, scope="function")
 def registry_mock(monkeypatch: pytest.MonkeyPatch):
     monkeypatch.setattr(mocktrics_exporter.metrics.Metric, "_registry", CollectorRegistry())
+    monkeypatch.setattr(
+        mocktrics_exporter.metaMetrics,
+        "metrics",
+        mocktrics_exporter.metaMetrics.Metrics(CollectorRegistry()),
+    )
 
 
 @pytest.fixture(autouse=True, scope="function")
 def clear_metrics(monkeypatch: pytest.MonkeyPatch):
-    monkeypatch.setattr(mocktrics_exporter.metricCollection.metrics, "_metrics", {})
+    monkeypatch.setattr(mocktrics_exporter.metricCollection.metrics, "_metrics", [])
 
 
 @pytest.fixture
